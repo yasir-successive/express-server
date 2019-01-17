76b233d2daab290f67e370637428ab6b5ee475be
yasirsuccessive <mohd.yasir@successive.tech>
2019-01-17 11:58:06 +0530

    Fix: r indentation error

--- extra/patterns/diamods.js
+++ extra/patterns/diamods.js
@@ -1,25 +1,17 @@
-function diamods(n)
-{
-    console.log('triangle for',n,'row');
-    let pattern='';
-    for(let i=1;i<=n;i++)
-    {
-        for(let j=n-1;j>=i;j--)
-        {
-        
-
-            pattern+=' ';
-        }
-
-        for(let j=1;j<=i;j++)
-        {
-        
-
-            pattern+='* ';
-        }
-        pattern+='\n';
+function diamods(n) {
+  console.log("triangle for", n, "row");
+  let pattern = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = n - 1; j >= i; j--) {
+      pattern += " ";
     }
-    console.log(pattern);
+
+    for (let j = 1; j <= i; j++) {
+      pattern += "* ";
+    }
+    pattern += "\n";
+  }
+  console.log(pattern);
 }
 
 diamods(10);
--- extra/patterns/equilateral.js
+++ extra/patterns/equilateral.js
@@ -1,42 +1,27 @@
-function equilateral(n)
-{
-    console.log('triangle for',n,'row');
-    let pattern='';
-    for(let i=1;i<=n;i++)
-    {
-        for(let j=n-1;j>=i;j--)
-        {
-        
-
-            pattern+=' ';
-        }
-
-        for(let j=1;j<=i;j++)
-        {
-        
-
-            pattern+='* ';
-        }
-        pattern+='\n';
+function equilateral(n) {
+  console.log("triangle for", n, "row");
+  let pattern = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = n - 1; j >= i; j--) {
+      pattern += " ";
     }
-    for(let i=n;i>=1;i--)
-    {
-        for(let j=n-1;j>=i;j--)
-        {
-        
 
-            pattern+=' ';
-        }
-
-        for(let j=1;j<=i;j++)
-        {
-        
-
-            pattern+='* ';
-        }
-        pattern+='\n';
+    for (let j = 1; j <= i; j++) {
+      pattern += "* ";
     }
-    console.log(pattern);
+    pattern += "\n";
+  }
+  for (let i = n; i >= 1; i--) {
+    for (let j = n - 1; j >= i; j--) {
+      pattern += " ";
+    }
+
+    for (let j = 1; j <= i; j++) {
+      pattern += "* ";
+    }
+    pattern += "\n";
+  }
+  console.log(pattern);
 }
 
 equilateral(5);
