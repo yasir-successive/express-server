fd8a16fad8ad0061e6fe1c6a79795cb2385f938d
yasirsuccessive <mohd.yasir@successive.tech>
2019-01-30 18:49:23 +0530

    feat:Intro to Mongoose and promises

--- src/config/IConfig.ts
+++ src/config/IConfig.ts
@@ -1,3 +1,4 @@
 export interface IConfig {
   port: string;
+  mongo:string;
 }
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,9 @@
+import { mongo } from 'mongoose';
 import { IConfig } from "./IConfig";
 import { config } from "dotenv";
 config();
 const configuration: IConfig = Object.freeze({
-  port: process.env.PORT
+  port: process.env.PORT,
+  mongo:process.env.MONGO_URL
 });
 export default configuration;
